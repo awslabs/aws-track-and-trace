118263f8caaab07c76b4107fb8104f7213e919c6
Pelayo Sanchez Margareto <pelaym@amazon.com>
2019-02-27 17:32:56 +0100

    chore: ignoring unneccessary files and adding bare footer

---
+++ dashboard/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="footer">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'footer',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.footer {
+}
+
+</style>
